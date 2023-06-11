55856b4ddf78a871346804be8bc2400645524245
Kumar Aditya <kumaraditya@cardbyte.ai>
2023-06-11 10:37:44 +0530

    All crud API are completed.

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express();
 require('./config/MongoDBConnection')
 const productRouter = require('./router/ProductRouter')
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-app.use('/product',productRouter)
-app.listen(5000,()=>{
-  console.log("server running on 5000")
+app.use('/product', productRouter)
+app.listen(5000, () => {
+    console.log("server running on 5000")
 })
