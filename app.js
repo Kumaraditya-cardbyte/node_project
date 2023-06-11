71afbff19ecb2be8b553eb4af5a0cfd94f9babfa
Kumar Aditya <kumaraditya@cardbyte.ai>
2023-06-11 15:41:46 +0530

    code has been fixed now after authentication redirection is happening

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const express = require('express')
 const app = express();
+const session = require('express-session')
 require('./config/MongoDBConnection')
+require('./auth/auth')
 const productRouter = require('./router/ProductRouter')
+const passport = require('passport')
+const store = require('store')
+const {localStorage} = require('node-localstorage')
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use('/product', productRouter)
+app.use('/product',isLoggedIn, productRouter)
+app.use(require('cookie-parser')());
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+function isLoggedIn(req,res,next){
+    req.headers['cookie'] ? next(): res.sendStatus(401)
+}
+
+app.get('/', (req, res) => {
+    const link = '<a href="/auth">Click here</a>';
+    res.send(
+       `Visit this page ${link}`
+    ).end()
+})
+
+app.get('/fail',(req,res)=>{
+    res.send("Unauthenticated user")
+    res.end();
+})
+
+app.get('/auth', passport.authenticate('google', {scope: ['email', 'profile']}))
+
+app.get('/google/auth/callback', passport.authenticate('google',{
+    successRedirect: '/product/',
+    failureRedirect: '/fail'
+}))
 app.listen(5000, () => {
     console.log("server running on 5000")
 })
